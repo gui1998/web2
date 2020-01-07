5f162a71bf3f1d9bb9848a74f7b20cbbd16549e7
Guilherme Mossi <guilhermeissom@outlook.com>
2020-01-07 18:08:17 -0300

    atualiza arquivos cdi para ajustes

---
+++ src/java/dao/ProdutoDAOImpl.java
@@ -0,0 +1,57 @@
+package dao;
+
+import cdi.DAOQualifier;
+import entidades.Cliente;
+import java.util.List;
+import javax.enterprise.event.Event;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+
+@DAOQualifier
+public class ProdutoDAOImpl implements ClienteDAO {
+
+    @Inject
+    private EntityManagerFactory fabrica;
+    
+    @Inject
+    private Event<Cliente> evento;
+
+    public void save(Cliente cliente) {
+        EntityManager em = fabrica.createEntityManager();
+        em.getTransaction().begin();
+        System.out.println(cliente.getClass().getSimpleName());
+        if (cliente.getId() > 0) {
+            em.merge(cliente);
+        } else {
+            em.persist(cliente);
+        }
+        em.getTransaction().commit();
+        em.close();
+        evento.fire(cliente);
+    }
+    
+    public void delete(Cliente cliente){
+        EntityManager em = fabrica.createEntityManager();
+        em.getTransaction().begin();
+        if(!em.contains(cliente)){
+            cliente = em.merge(cliente);
+        }
+        em.remove(cliente);
+        em.getTransaction().commit();
+        em.close();
+    }
+    
+    public Cliente find(int id){
+        EntityManager em = fabrica.createEntityManager();
+        return em.find(Cliente.class, id);
+    }
+    
+    public List<Cliente> list(){
+        EntityManager em = fabrica.createEntityManager();
+        Query q = em.createQuery("SELECT c FROM Cliente c");
+        return q.getResultList();
+    }
+
+}
---
+++ src/java/servlet/VeiculoCDIServlet.java
@@ -0,0 +1,92 @@
+package servlet;
+
+import cdi.DAOQualifier;
+import dao.ClienteDAO;
+import dao.ClienteDAOImpl;
+import entidades.Cliente;
+import java.io.IOException;
+import javax.inject.Inject;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "ClienteCDIServlet", urlPatterns = {"/clientecdi"})
+public class VeiculoCDIServlet extends HttpServlet {
+    
+    @Inject @DAOQualifier
+    private ClienteDAO dao;
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        Cliente c = new Cliente();
+
+        if (request.getParameter("nome") != null) {
+            if(!request.getParameter("id").equals("")){
+                int id = Integer.parseInt(request.getParameter("id"));
+                c.setId(id);
+            }
+            c.setNome(request.getParameter("nome"));
+            c.setCpf(request.getParameter("cpf"));
+            c.setTelefone(request.getParameter("telefone"));
+            dao.save(c);
+        } else if (request.getParameter("excluir") != null) {
+            int id = Integer.parseInt(request.getParameter("excluir"));
+            dao.delete(dao.find(id));
+        } else if (request.getParameter("editar") != null) {
+            int id = Integer.parseInt(request.getParameter("editar"));
+
+            request.setAttribute("cliente", dao.find(id));
+        }
+
+        request.setAttribute("lista", dao.list());
+
+        RequestDispatcher view = request.getRequestDispatcher("clientecdi.jsp");
+        view.forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
